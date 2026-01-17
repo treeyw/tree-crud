5a9fd86096e0ccba68ca330d4150a14ec2262db7
yuehao <yuehaoly@163.com>
2026-01-17 18:33:53 +0800

    更新pom，替换掉漏洞依赖

--- src/main/java/io/github/treeyw/crud/dao/jpa/filter/DaoFiltel.java
+++ src/main/java/io/github/treeyw/crud/dao/jpa/filter/DaoFiltel.java
@@ -2,7 +2,7 @@ package io.github.treeyw.crud.dao.jpa.filter;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.EmptyInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
